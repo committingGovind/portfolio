e853b82c413089411729d45fea41291b2215a5da
committingGovind <shuklagovind4825@gmail.com>
2024-05-09 01:54:53 +0530

    speed run analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { Analytics } from "@vercel/analytics/react";
 import "./App.css";
 import { Background } from "./components/Background";
-
+import { SpeedInsights } from "@vercel/speed-insights/react"
 function App() {
   return (
     <div>
       <Background />
-      <Analytics/>
-      
+      <Analytics />
+      <SpeedInsights />
+
       {/* <Video /> */}
     </div>
   );
